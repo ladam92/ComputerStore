aa234493e825a448a04d21ea4e7ca3fc2d719c12
Liska Ádám <adam.liska@me.com>
2015-10-18 11:30:51 +0200

    project struktúra

---
+++ ComputerStore/ComputerStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ComputerStore.Startup))]
+namespace ComputerStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
