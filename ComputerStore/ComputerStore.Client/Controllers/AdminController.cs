e026bf9bfab4f044310ba27f19a8e26a2e763bc9
ladam92 <caesar456@gmail.com>
2015-11-23 19:14:04 +0100

    admin

---
+++ ComputerStore/ComputerStore.Client/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComputerStore.Client.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EditCategories()
+        {
+            return View();
+        }
+
+        public ActionResult GetCategories(int pageNum)
+        {
+            return PartialView();
+        }
+    }
+}
